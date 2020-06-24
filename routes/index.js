0f120aa95e8bee80228b1af820a948f53e08b3ad
lzdev <lzdev>
2020-06-24 11:54:13 +0100

    server validation for contact form

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,24 @@ router.get("/contact", (req, res) => {
 });
 
 router.post("/contact", (req, res) => {
+
+  const {name, contactEmail, message } = req.body;
+
+  // Server validation for contact
+  if (!name || !message || !validateEmail(contactEmail)) {
+    req.flash('error', 'Oops. Something went wrong sending you message. Please try again later.');
+    return res.redirect('/');
+  }
+
   const mailOptions = {
     from: "lzdev20@***REMOVED***.com",
     to: "lzdev20@***REMOVED***.com",
     subject: "Message from Website",
-    text: `Message from ${req.body.email}:
+    text: `Message from ${name}, ${contactEmail}:
     
-    ${req.body.message}`
+    ${message}`
   };
+
 
   transporter
     .sendMail(mailOptions)
@@ -36,6 +46,12 @@ router.post("/contact", (req, res) => {
       res.redirect("/");
     });
 });
+
+function validateEmail(email) {
+  const patt = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  const result = patt.test(email);
+  return result;
+}
 
 
 module.exports = router;

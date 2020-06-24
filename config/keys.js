e9e61c62c5e30808925e9ee2a241ffb3229e5339
lzdev <lzdev>
2020-06-24 12:03:15 +0100

    Server validation for contact

---
+++ config/keys.js
@@ -0,0 +1,20 @@
+module.exports = {
+    passport: {
+        google: {
+            clientID: '868180603727-a265ilfl0hidlvotsmgrr9ckk4iv4mrm.apps.googleusercontent.com',
+            clientSecret: '9kXSztYte50MsDP3PGEaJk2s'
+        },
+        facebook: {
+            clientID: ***REMOVED***,
+            clientSecret: 'bb7796cb6215fe6e81eb7b1cce36d319'
+        }
+    },
+    expressSession: {
+        secret: 'This is my Super Secret Key'
+    },
+    nodemailer: {
+        user: 'lzdev20@***REMOVED***.com',
+        pass: 'Testio1234567'
+    }
+
+}

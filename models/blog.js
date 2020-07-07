3c499e40ab4746d7a03dc41d5d599ef641a087b4
LZDev <@lzdev>
2020-07-07 10:59:45 +0100

    blog.js

--- models/blog.js
+++ models/blog.js
@@ -25,14 +25,4 @@ const Blog = new mongoose.model('Blog', BlogSchema);
 //     .then(result => console.log(result))
 //     .catch(err => console.log(err));
 
-<<<<<<< HEAD
-// Blog.create({
-//     title: "Blog 1 about something junior developery",
-//     text: "This is blog text"
-// })
-//     .then(savedBlog => console.log("Blog saved: " + savedBlog))
-//     .catch(err => console.log(err));
-
-=======
->>>>>>> dotenv
-module.exports = Blog;
+module.exports = Blog; 

4706673a54a198295bd9f9335cf67cf00a60c46d
LZDev <@lzdev>
2020-07-14 09:36:23 +0100

    fixed about link click from other page

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,4 @@
+// set top margin fix
 function setTopMargin() {
   $("body").css("margin-top", $("#mainNav").outerHeight(true));
 }
@@ -6,20 +7,36 @@ $(document).ready(setTopMargin());
 
 $(window).resize(setTopMargin());
 
+
 // scroll to about without nav hiding title
 if (document.getElementById("about")) {
   const aboutLink = document.querySelector("a[href='/#about']");
 
-  aboutLink.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    const aboutTarget = document.getElementById("about");
-    const navHeight = document.getElementById("mainNav").clientHeight;
-
-    let { x, y } = aboutTarget.getBoundingClientRect();
-    x += window.scrollX;
-    y += window.scrollY;
-
-    window.scroll(x, y - navHeight);
-  });
+  aboutLink.addEventListener("click", scrollToAbout);
 }
+
+function scrollToAbout(e) {
+  if (e) {
+    e.preventDefault();
+  }
+
+  const aboutTarget = document.getElementById("about");
+  const navHeight = document.getElementById("mainNav").clientHeight;
+
+  let { x, y } = aboutTarget.getBoundingClientRect();
+  x += window.scrollX;
+  y += window.scrollY;
+
+  window.scroll(x, y - navHeight);
+}
+
+// if about link clicked from other pages
+window.onload = () => {
+  if (location.hash && location.hash === '#about') {
+    location.hash = "";
+    scrollToAbout();
+  }
+}
+
+
+
